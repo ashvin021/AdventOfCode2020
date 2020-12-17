17c865de067ff97478924f30a11008e306cec979
Ashvin Arsakularatne <ashvin.arsakularatne@gmail.com>
2020-12-17 22:32:19 +0530

    Added intersection function

--- Reusable.hs
+++ Reusable.hs
@@ -1,7 +1,11 @@
 module Reusable where
 
+import Data.List ( intersect )
 
 (.:) = (.).(.)
 
 uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
 uncurry3 f (x, y, z) = f x y z
+
+intersection :: Eq a => [[a]] -> [a]
+intersection = foldr1 intersect
