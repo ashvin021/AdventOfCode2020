830951a72b551bfe2ee8305a98adece7047356ea
Ashvin Arsakularatne <ashvin.arsakularatne@gmail.com>
2020-12-17 22:30:15 +0530

    Completed Day 15

--- Day15.hs
+++ Day15.hs
@@ -1,8 +1,12 @@
 module Day15 where
 
 import Data.MemoTrie
+import Data.Bool ( bool )
 import Data.Maybe ( fromJust, isNothing )
 import Data.Function ( fix )
+import Control.Monad ( foldM, forM_ )
+import Control.Monad.ST ( runST )
+import qualified Data.Vector.Unboxed.Mutable as V
 import qualified Data.IntMap.Lazy as IM
 
 
@@ -23,6 +27,8 @@ q1 n
 
 
 q2Table = scanl (\m x -> IM.insert (memoQ2 x) x m) IM.empty [1..]
+q2Table' f n = if n == 0 then IM.empty else IM.insert (memoQ2 n) n (f (n - 1))
+q2TableMemo = memoFix q2Table'
 
 q2 :: (Int -> Int) -> Int -> Int
 q2 f n
@@ -30,11 +36,34 @@ q2 f n
   | isNothing lastN          = 0
   | otherwise                = (n - 1) - fromJust lastN
   where
---    prevList = take 1 . dropWhile ((/=prev) . snd) $ zip desc (map f desc)
     prev     = f (n - 1)
---    lastN    = fst . head $ prevList
-    lastN    = (q2Table !! (n - 2)) IM.!? prev
---    desc     = [n - 2, n - 3 .. 1]
-
+    lastN    = q2TableMemo (n - 2) IM.!? prev
 
 memoQ2 = memoFix q2
+
+
+
+
+
+
+
+---------------------------------------------------------------------------
+-- This version is heavily inspired by the solution at cdParks/advent2020
+-- Probably going to retry this later without mutable vectors
+
+q2' :: Int -> Int
+q2' n = runST $ do
+
+  memo <- V.new (max n (length startNumbers))
+  forM_ startMap (uncurry $ V.write memo)
+  
+  foldM (\prev currentTurn -> do
+    lastN <- V.unsafeRead memo prev
+    V.unsafeWrite memo prev currentTurn
+    pure $ if lastN == 0 then 0 else currentTurn - lastN
+        ) (last startNumbers) turnsLeft
+
+  where
+    startMap  = zip startNumbers [1..]
+    turnsLeft = [length startNumbers .. pred n]
+
